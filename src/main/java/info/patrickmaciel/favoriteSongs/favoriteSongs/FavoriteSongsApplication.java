76abb069b37cb4a504b54161aeeb5c41a29f9739
Patrick Maciel <patrickmaciel.info@gmail.com>
2024-11-26 18:07:31 -0300

    initial commit

---
+++ src/main/java/info/patrickmaciel/favoriteSongs/favoriteSongs/FavoriteSongsApplication.java
@@ -0,0 +1,13 @@
+package info.patrickmaciel.favoriteSongs.favoriteSongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteSongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteSongsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/patrickmaciel/favoriteSongs/favoriteSongs/FavoriteSongsApplicationTests.java
@@ -0,0 +1,13 @@
+package info.patrickmaciel.favoriteSongs.favoriteSongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteSongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
